03d0597ed15b8514f344ac8fe453f359610b5662
feraway <ferawayfromhere@gmail.com>
2023-08-12 13:40:51 -0300

    feat(): redirect to root if wallet not connected

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useAccount } from "wagmi";
 import Grid from "@mui/material/Grid";
 import Header from "components/Header";
 
 function Layout() {
+  const { address } = useAccount();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  console.log("logger", { address, location, navigate });
+
+  useEffect(() => {
+    if (!address && pathname !== "/") {
+      console.log("logger redirecting");
+      navigate("/");
+    }
+  }, [navigate, pathname, address]);
+
   return (
     <Grid container justifyContent="center">
-      <Grid item container flexDirection="column" alignItems="center" xs={6}>
+      <Grid item container flexDirection="column" alignItems="center" xs={10}>
         <Grid item>
           <Header />
         </Grid>
